ce191b11a314657a3ed84732f9bb932d3e442d27
salmsrocks <58145915+salmsrocks@users.noreply.github.com>
2020-03-06 18:38:13 +0530

    Update alb.tf
    changes done for my project

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -8,7 +8,7 @@ resource "aws_alb" "main" {
 
 resource "aws_alb_target_group" "app" {
   name        = "cb-target-group"
-  port        = 80
+  port        = 9004
   protocol    = "HTTP"
   vpc_id      = aws_vpc.main.id
   target_type = "ip"
